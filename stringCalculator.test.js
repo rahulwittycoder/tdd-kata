b5f42f2653fea2c32d537f73a6762c3f9bac1092
Rahul Raj Tripathi <31700666+rahulwittycoder@users.noreply.github.com>
2025-03-21 08:31:21 +0530

    Added 1 pass test Case

---
+++ stringCalculator.test.js
@@ -0,0 +1,6 @@
+const StringCalculator = require("./stringCalculator");
+
+test("returns 0 for an empty string", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("")).toBe(0);
+});
